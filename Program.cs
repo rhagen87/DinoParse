59c726e55b596190bcd4b072df610ab06e2d8dab
rhagen87 <reynold.hagen@gmail.com>
2021-01-28 18:43:09 -0600

    Initial Development Code 1/28/21

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net;
+namespace DinoParse
+{
+    class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Enter the name of a Wikipedia page to find related links:");
+            string userSearch = Console.ReadLine();
+            string searchLink = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=links&redirect=1&page=" + userSearch;
+            string path = @"DinoParse.csv";
+            dynamic link = RetrieveJson(searchLink);
+            string linkString = link.ToString();
+            File.WriteAllText(path, linkString);
+            Console.WriteLine(linkString);
+        }
+
+        public static dynamic RetrieveJson(string input)
+        {
+            using WebClient wc = new WebClient();
+            string json = wc.DownloadString(input);
+            JObject dinoList = JObject.Parse(json);
+            JArray links = (JArray)dinoList["parse"]["links"];
+            JArray parsedLinks = new JArray();
+            foreach (JObject link in links)
+            {
+                string linkString = link["ns"].ToString();
+                if (linkString == "0")
+                {
+                    parsedLinks.Add(link["*"]);
+                }
+            }
+            return parsedLinks;
+        }
+    }
+}
